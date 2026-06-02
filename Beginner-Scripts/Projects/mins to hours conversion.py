3deed53ca02d12bf7056d3a20c4789beb4d102b2
erenose <onotoleerenose@gmail.com>
2026-06-01 22:08:21 -0400

    Worked on a min to hour conversion application

---
+++ Beginner-Scripts/Projects/mins to hours conversion.py
@@ -0,0 +1,12 @@
+
+total_min = int(input('Total Mins worked today? '))
+hour = total_min // 60 # divides min/60 for the hour
+remaining_min = total_min % 60 # "//" gets the remaining units "min"
+if hour < 4: # if hour is less than 4 hours
+    print('Part-Time Short Shift')
+elif 4 <= hour < 8: # if 4 is greater than or equal to hour and less than 8 
+    print('Standard Full-Time Shift')
+else:   # anything greater than 8 
+    print('Overtime Shift detected')
+print(f'{hour} hr and {remaining_min} minutes')
+
