7df62ce6ac8a48baa8c40e5b48d1f3bf575a24af
erenose <onotoleerenose@gmail.com>
2026-06-03 18:36:04 -0400

    Matrix

---
+++ Beginner-Scripts/Matrix.py
@@ -0,0 +1,12 @@
+
+# Matrix Stores all the numbers
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# For each of the 3 rows in the matrix
+for row in matrix:
+# Print out the items/numbers in each row
+    for item in row:
+        print(item)
