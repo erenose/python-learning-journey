05539cf98cdf602478a2c4d0728e62f98bc9473c
erenose <onotoleerenose@gmail.com>
2026-06-02 21:36:44 -0400

    learning for loops

---
+++ Beginner-Scripts/for loops.py
@@ -0,0 +1,11 @@
+
+for item in range(5, 10, 2): # 2 at the ends skips the number from 5 to 9 by 2
+    print(item)
+
+
+# practicing 
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price
+print(f'{total}')
