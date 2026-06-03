08726e8b3571d1932a7ae5b2bac311469c0b73e2
erenose <onotoleerenose@gmail.com>
2026-06-03 17:19:37 -0400

    Practicing for loops

--- Beginner-Scripts/for loops.py
+++ Beginner-Scripts/for loops.py
@@ -2,9 +2,14 @@
 for item in range(5, 10, 2): # 2 at the ends skips the number from 5 to 9 by 2
     print(item)
 
-# practicing 
+
+# practicing
+
+# The list
 prices = [10, 20, 30]
 total = 0
 for price in prices:
+# adds the total to the prices 10+0, 20+0 and 30+0, when done it adds the mall together
     total += price
+# Then prints out the total
 print(f'{total}')
