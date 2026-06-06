fef37f3c7f39b9ec28b9c7dca78a1bcf78870ad6
erenose <onotoleerenose@gmail.com>
2026-06-05 20:41:50 -0400

    inventory checker

---
+++ Beginner-Scripts/Projects/inventory checker.py
@@ -0,0 +1,10 @@
+
+# list
+backpacks = ['Sword', 'Shield' , 'Potion' , 'Potion' , 'Key' , 'Bread']
+item = ''
+# for items in the list
+for backpack in backpacks:
+# print it out
+    print('item:', backpack)
+# show how many items left in list 
+print('Total items left:', len(backpacks))
