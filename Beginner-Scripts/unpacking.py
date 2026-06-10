483f564f35050cdc868dff97a50298259c2e2de9
erenose <onotoleerenose@gmail.com>
2026-06-10 15:03:52 -0400

    unpacking

---
+++ Beginner-Scripts/unpacking.py
@@ -0,0 +1,14 @@
+
+# Unpacking extracts info from a table and allocates it to a specific variable. 
+
+# List the info in coordinates 
+coordinates = (1, 2, 3)
+# Allocate x,y,z to "coordinates"
+x, y, z = coordinates
+print(x, y, z)
+
+# List the info in coordinates 
+list = [1, 2, 3]
+# Allocate H,L,C to "list"
+H, L, C = list
+print(H, L, C)
