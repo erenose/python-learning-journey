fed58581dbbf21c094d739e23ac1e34c5781adf4
ere. <onotoleerenose@gmail.com>
2026-06-11 17:20:21 -0400

    parameters

---
+++ Beginner-Scripts/parameters.py
@@ -0,0 +1,13 @@
+
+# Assigns greet user/what to print out
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome Aboard')
+
+# This "Start" prints out first
+print("start")
+# Then it prints out what's assigned to greet_user
+greet_user("John", "Doe")
+greet_user("Michael", "Jackson")
+# Prints "Finish"
+print('Finish')
