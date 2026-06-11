99bef2f0b38af74a628a352cc2aaf5948128591f
ere. <onotoleerenose@gmail.com>
2026-06-11 17:16:32 -0400

    return statements

---
+++ Beginner-Scripts/return statements.py
@@ -0,0 +1,13 @@
+
+# the square defines the number
+def square(number):
+    # this squares the number
+    return number * number
+
+# we then set the square at 5 and store as "result"
+result = square(5)
+# prints the answer
+print(result)
+
+# or
+print(square(5))
