2e2c9c7809e4152c945c531034f1e0decab92b16
ere. <onotoleerenose@gmail.com>
2026-06-11 17:18:04 -0400

    function

---
+++ Beginner-Scripts/function.py
@@ -0,0 +1,12 @@
+
+# Assigns greet user/what to print out
+def greet_user():
+    print('Hi there!')
+    print('Welcome Aboard')
+
+# This "Start" prints out first
+print("start")
+# Then it prints out what's assigned to greet_user
+greet_user()
+# Prints "Finish"
+print('Finish')
