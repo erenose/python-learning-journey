748405032557e4e7ecfb7c3546d38a6c55dea7d7
ere. <onotoleerenose@gmail.com>
2026-06-11 17:19:53 -0400

    key-word arguments

---
+++ Beginner-Scripts/key-word arguments.py
@@ -0,0 +1,12 @@
+
+# Assigns greet user/what to print out
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome Aboard')
+
+# This "Start" prints out first
+print("start")
+# Then it prints out what's assigned to greet_user
+greet_user("Michael", first_name="Jackson")
+# Prints "Finish"
+print('Finish')
